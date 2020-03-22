e7554b5bccdfcb5c1735792ae9073b8a42ff5d66
phkl <phkl@tuta.io>
2020-03-22 20:37:24 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let brackets = bracketsConfig[i].join('');
+    if (str.includes(brackets)) {
+      str = str.replace(brackets, '');
+      i = -1;
+    }
+  }
+    
+  if (str.length != 0) {
+    result = false;
+  }
+  else {
+    result = true;
+  }
+  return result;
 }
